f18c9f18283e1f5b973da2a95f559f8e18f9c1c8
noothanks <coreyfuller727@yahoo.com>
2022-05-04 18:37:31 -0400

    Adding user input to new task and refactoring submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,26 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-  
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give div element a class name
+    taskInfoE1.className = "task-info";
+    //Add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoE1);
+    
+    //add entire item to list
     tasksToDoE1.appendChild(listItemEl);
 
+    console.dir(listItemEl);
   };
 
 formE1.addEventListener("submit", createTaskHandler);
